68ecf0ffa89647b4994b913b9572ffbcd8176b06
Evgeny Novgorodov <greatly01@ya.ru>
2020-12-24 09:57:08 +0500

    Initial Commit

---
+++ PersonListsHW/PersonListsHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonListsHWApp.swift
+//  PersonListsHW
+//
+//  Created by User on 24.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct PersonListsHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
